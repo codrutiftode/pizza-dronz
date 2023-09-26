77a58234dca453fb8f5bf75f57fd2834dc795771
codrutiftode <codriniftode@gmail.com>
2023-09-26 16:13:51 +0100

    Add stubs for the two interfaces needed in CW1

---
+++ src/main/java/uk/ac/ed/inf/OrderValidation.java
@@ -0,0 +1,11 @@
+package uk.ac.ed.inf;
+
+import uk.ac.ed.inf.ilp.data.Order;
+import uk.ac.ed.inf.ilp.data.Restaurant;
+
+public class OrderValidation implements uk.ac.ed.inf.ilp.interfaces.OrderValidation {
+    @Override
+    public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants) {
+        return null;
+    }
+}
