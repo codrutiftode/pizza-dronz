d569f0321b741cdcaac175e4db822e060a4f0741
codrutiftode <codriniftode@gmail.com>
2023-10-08 15:23:00 +0100

    Create basic structure for Order Validator

--- src/main/java/uk/ac/ed/inf/OrderValidator.java
+++ src/main/java/uk/ac/ed/inf/OrderValidator.java
@@ -1,11 +1,101 @@
 package uk.ac.ed.inf;
 
+import uk.ac.ed.inf.ilp.constant.OrderStatus;
+import uk.ac.ed.inf.ilp.constant.OrderValidationCode;
+import uk.ac.ed.inf.ilp.data.CreditCardInformation;
 import uk.ac.ed.inf.ilp.data.Order;
+import uk.ac.ed.inf.ilp.data.Pizza;
 import uk.ac.ed.inf.ilp.data.Restaurant;
 
 public class OrderValidator implements uk.ac.ed.inf.ilp.interfaces.OrderValidation {
+
+    private boolean isValidCardNumber(String cardNumber) {
+        return cardNumber.length() == 16;
+    }
+
+    private boolean isValidCardCVV(String cvv) {
+        return cvv.length() == 3;
+    }
+
+    private boolean isValidCardExpiry(String expiry) {
+        return true; // TODO
+    }
+
+    private boolean isTotalPriceCorrect(Pizza[] pizzas, int totalInPence) {
+        int sum = 0;
+        for (Pizza pizza : pizzas) sum += pizza.priceInPence();
+        return sum == totalInPence;
+    }
+
+    private boolean allPizzasDefined(Pizza[] pizzas, Restaurant[] restaurants) {
+        return true; // TODO
+    }
+
+    private boolean tooManyPizzas(Pizza[] pizzas) {
+        return pizzas.length > 4;
+    }
+
+    private boolean isMultipleRestaurants(Pizza[] pizzas, Restaurant[] restaurants) {
+        return true; // TODO
+    }
+
     @Override
     public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants) {
-        return null;
+        CreditCardInformation cardInfo = orderToValidate.getCreditCardInformation();
+
+        // Test card number
+        if (!isValidCardNumber(cardInfo.getCreditCardNumber())) {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.CARD_NUMBER_INVALID);
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
+            return orderToValidate;
+        }
+
+        // Test card expiry
+        if (!isValidCardExpiry(cardInfo.getCreditCardExpiry())) {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.EXPIRY_DATE_INVALID);
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
+            return orderToValidate;
+        }
+
+        // Test card CVV
+        if (!isValidCardCVV(cardInfo.getCvv())) {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.CVV_INVALID);
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
+            return orderToValidate;
+        }
+
+        // Test total price
+        if (!isTotalPriceCorrect(orderToValidate.getPizzasInOrder(),
+                orderToValidate.getPriceTotalInPence())) {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.TOTAL_INCORRECT);
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
+            return orderToValidate;
+        }
+
+        // Test all pizzas are defined and belong to one restaurant
+        if (!allPizzasDefined(orderToValidate.getPizzasInOrder(), definedRestaurants)) {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.PIZZA_NOT_DEFINED);
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
+            return orderToValidate;
+        }
+
+        // Test number of pizzas does not exceed maximum count
+        if (tooManyPizzas(orderToValidate.getPizzasInOrder())) {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.MAX_PIZZA_COUNT_EXCEEDED);
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
+            return orderToValidate;
+        }
+
+        // Test if pizza was ordered from multiple restaurants
+        if (isMultipleRestaurants(orderToValidate.getPizzasInOrder(), definedRestaurants)) {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.PIZZA_FROM_MULTIPLE_RESTAURANTS);
+            orderToValidate.setOrderStatus(OrderStatus.INVALID);
+            return orderToValidate;
+        }
+
+        // No error found, pizza is valid
+        orderToValidate.setOrderValidationCode(OrderValidationCode.NO_ERROR);
+        orderToValidate.setOrderStatus(OrderStatus.VALID_BUT_NOT_DELIVERED);
+        return orderToValidate;
     }
 }
