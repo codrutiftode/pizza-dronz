cbf81cddfe1ecfaca7f074198c4a89066eb865b0
codrutiftode <codriniftode@gmail.com>
2023-11-07 14:37:53 +0000

    Make App use APIClient

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,5 +1,9 @@
 package uk.ac.ed.inf;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import uk.ac.ed.inf.ilp.data.Restaurant;
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        CustomLogger logger = CustomLogger.getLogger();
+        if (args.length != 2) {
+            logger.error("Usage: <jar-file> <target-date> <API-URL>.");
+            return;
+        }
+
+        String date = args[0];
+        String apiUrl = args[1];
+
+        OrdersAPIClient apiClient = new OrdersAPIClient(apiUrl);
+        boolean isAlive = apiClient.checkAliveAPI();
+
+        if (isAlive) {
+            try {
+                Restaurant[] restaurants = apiClient.getRestaurants();
+                logger.log(Arrays.toString(restaurants));
+            }
+            catch(JsonProcessingException e) {
+                logger.error("There was an error when parsing JSON data from the API.");
+                return;
+            }
+
+        }
     }
 }
